5b6b7c1d8f9a0ac17cf01da2718a312bd66095cc
Anisha Singhal <anisha.singhal@gmail.com>
2021-02-14 12:12:03 -0500

    Uploading for Jenkin

---
+++ src/test/java/testCases/LoginPageTest.java
@@ -0,0 +1,60 @@
+package testCases;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.TestBase;
+import page.DataBasePage;
+import page.LoginPage;
+
+public class LoginPageTest extends TestBase{
+	LoginPage loginpageObj;
+	
+	@BeforeMethod
+	 public void setUp() {
+	  initializeDriver();
+	  loginpageObj = PageFactory.initElements(driver, LoginPage.class);
+	 }
+	
+	@Test(priority=1)
+	public void logintest() throws InterruptedException, ClassNotFoundException, SQLException, IOException {
+		loginpageObj.enterUserName("demo@techfios.com");
+		loginpageObj.enterPassword("abc123");
+//		loginpageObj.enterUserName(DataBasePage.getData("username"));
+//		loginpageObj.enterPassword(DataBasePage.getData("password"));
+		loginpageObj.clickSignInButton();
+		Thread.sleep(3000);
+		
+		takeScreenshotAtEndOfTest(driver);
+		
+	}
+	
+	@Test(priority=2)
+	public void logintest2() throws InterruptedException, ClassNotFoundException, SQLException, IOException {
+		loginpageObj.enterUserName("demo@techfios.com");
+		loginpageObj.enterPassword("abc123");
+//		loginpageObj.enterUserName(DataBasePage.getData("username"));
+//		loginpageObj.enterPassword(DataBasePage.getData("password"));
+		loginpageObj.clickSignInButton();
+		Thread.sleep(3000);
+		
+		 String expectedTitle="Dashboard- iBilling";
+		  String actualTitle=loginpageObj.getPageTitle();
+		  System.out.println(actualTitle);
+		  
+		 Assert.assertEquals(expectedTitle, actualTitle);
+	}
+	
+	@AfterMethod
+	 public void tearDown() {  
+	  driver.close();
+	  driver.quit();
+	 }
+	
+}
